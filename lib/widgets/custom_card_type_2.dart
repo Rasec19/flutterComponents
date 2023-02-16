ab590f975c94b0de8809e6461269a3e029cd60bc
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-16 16:56:55 -0700

    update

---
+++ lib/widgets/custom_card_type_2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomCardType2 extends StatelessWidget {
+  const CustomCardType2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: const [
+          Image(
+              image: NetworkImage(
+                  'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'))
+        ],
+      ),
+    );
+  }
+}
