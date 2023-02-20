ed0aca42556b3137a12ddeb18ca35222c9ec80ee
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-20 16:54:06 -0700

    updated

---
+++ lib/screens/inputs_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InputsScreen extends StatelessWidget {
+  const InputsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs y Forms'),
+      ),
+      body: Center(
+        child: Text('InputsScreen'),
+      ),
+    );
+  }
+}
