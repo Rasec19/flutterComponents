0a3012c8750ca7912e49fa24410346ebb8353e3f
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-20 12:24:03 -0700

    update

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,14 +7,25 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
       elevation: 10,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage(
                 'https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg'),
             placeholder: AssetImage('assets/jar-loading.gif'),
-          )
+            width: double.infinity,
+            height: 330,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: const Text('Un hermoso paisaje'))
         ],
       ),
     );
