3eb776a25155ddf8010f2396d4109133d9b03e22
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-21 14:00:51 -0700

    update

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -6,8 +6,14 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
     final Map<String, String> formValues = {
       'first_name': 'Rasec',
+      'last_name': 'Camacho',
+      'email': 'rasec.camacho19@gmail.com',
+      'password': '123456',
+      'role': 'Admin',
     };
 
     return Scaffold(
@@ -18,33 +24,76 @@ class InputsScreen extends StatelessWidget {
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Form(
+              key: myFormKey,
               child: Column(
                 children: [
-                  const CustomInputField(
+                  CustomInputField(
                     labelText: 'Nombre',
                     hintText: 'Nombre del usuario',
+                    formProperty: 'first_name',
+                    formValues: formValues,
                   ),
                   const SizedBox(height: 30),
-                  const CustomInputField(
+                  CustomInputField(
                     labelText: 'Apellido',
                     hintText: 'Apellido del usuario',
+                    formProperty: 'last_name',
+                    formValues: formValues,
                   ),
                   const SizedBox(height: 30),
-                  const CustomInputField(
+                  CustomInputField(
                     labelText: 'Correo',
                     hintText: 'Correo del usuario',
                     keyboardType: TextInputType.emailAddress,
+                    formProperty: 'email',
+                    formValues: formValues,
                   ),
                   const SizedBox(height: 30),
-                  const CustomInputField(
+                  CustomInputField(
                     labelText: 'Contraseña',
                     hintText: 'Contraseña del usuario',
                     obscureText: true,
+                    formProperty: 'password',
+                    formValues: formValues,
+                  ),
+                  const SizedBox(height: 30),
+                  DropdownButtonFormField<String>(
+                    value: 'Admin',
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'Admin',
+                        child: Text('Admin'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Superuser',
+                        child: Text('Superuser'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Developer',
+                        child: Text('Developer'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Jr. Developer',
+                        child: Text('Jr. Developer'),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      print(value);
+                      formValues['role'] = value ?? 'Admin';
+                    },
                   ),
                   const SizedBox(height: 30),
                   ElevatedButton(
                       onPressed: () {
-                        //TODO: imprimir valores del formulario
+                        FocusScope.of(context).requestFocus(FocusNode());
+
+                        if (!myFormKey.currentState!.validate()) {
+                          print('Formulario no valido');
+                          return;
+                        }
+
+                        //* imprimir valores del formulario
+                        print(formValues);
                       },
                       child: const SizedBox(
                           width: double.infinity,
--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -18,14 +18,17 @@ class CustomInputField extends StatelessWidget {
     this.suffixIcon,
     this.keyboardType,
     this.obscureText = false,
+    required this.formProperty,
+    required this.formValues,
   }) : super(key: key);
+
+  final String formProperty;
+  final Map<String, String> formValues;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      onChanged: (value) {
-        print(value);
-      },
+      onChanged: (value) => formValues[formProperty] = value,
       validator: (value) {
         if (value == null) return 'Este campo es requerido';
         return value.length < 3 ? 'Minimo de 3 caracteres' : null;
