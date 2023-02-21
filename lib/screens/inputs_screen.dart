e418d8c20d0d4cb800747d652f4dbe77a10f163e
Rasec Camacho <rasec.camacho19@gmail.com>
2023-02-20 23:51:10 -0700

    update

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -7,10 +8,37 @@ class InputsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inputs y Forms'),
+        title: const Text('Inputs y Forms'),
       ),
-      body: Center(
-        child: Text('InputsScreen'),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              children: const [
+                CustomInputField(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre del usuario',
+                ),
+                SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Apellido',
+                  hintText: 'Apellido del usuario',
+                ),
+                SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Correo',
+                  hintText: 'Correo del usuario',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña del usuario',
+                  obscureText: true,
+                ),
+                SizedBox(height: 30),
+              ],
+            )),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,27 +4,46 @@ class AppTheme {
   static const Color primary = Colors.green;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //Color primario
-      primaryColor: Colors.indigo,
+    //Color primario
+    primaryColor: Colors.indigo,
 
-      //AppBar Theme
-      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+    //AppBar Theme
+    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
 
-      //TextButton Theme
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(foregroundColor: primary),
-      ),
+    //TextButton Theme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: primary),
+    ),
 
-      // FloatingActionButtons
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: primary, elevation: 0),
+    // FloatingActionButtons
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: primary, elevation: 0),
 
-      // ElevatedButtonTheme
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: primary,
-              shape: const StadiumBorder(),
-              elevation: 0)));
+    // ElevatedButtonTheme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: primary,
+            shape: const StadiumBorder(),
+            elevation: 0)),
+
+    // InputDecorationTheme}
+    inputDecorationTheme: const InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primary),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primary),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10))),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primary),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10))),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10)))),
+  );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
       //Color primario
